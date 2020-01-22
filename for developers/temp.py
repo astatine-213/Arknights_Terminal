6679f997e93c84050314ea97f660f420c88208d1
astatine-213 <1161451204@qq.com>
2020-01-23 00:02:43 +0800

    normal renew

--- for developers/temp.py
+++ for developers/temp.py
@@ -1,1 +1,12 @@
-
+with open("data.xp1",encoding="utf-8") as f:
+	tmp=f.read();f.close()
+tmp1=tmp.split("\n")[:-1];out=[]
+for x in tmp1:
+	x+=",,,,,"
+	out.append(x.replace("],,,,,,",",[],[],,],"))
+print(out)
+msg=""
+for x in out:
+	msg+=x+"\n"
+with open("data.xp1","w",encoding="utf-8") as f:
+	tmp=f.write(msg);f.close()
