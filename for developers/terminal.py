30146ba3a602749ec35011926a862eacd456deb0
astatine-213 <1161451204@qq.com>
2020-01-24 22:50:52 +0800

    normal renew

--- for developers/terminal.py
+++ for developers/terminal.py
@@ -11,7 +11,7 @@
 # organizationtuple=("罗德岛","喀兰贸易","龙门","莱茵生命","格拉斯哥帮","使徒","汐斯塔","深海猎人","SWEEP","乌萨斯学生自治团","王者之杖","企鹅物流","黑钢国际","维多利亚","卡西米尔无胄盟","莱塔尼亚")
 # drawertuple=("Liduke","竜崎いち","虎三","Infukun","海猫络合物","Skade","NoriZC","m9nokuro","一立里子","谜肘","TOKI","neco","阿鬼","唯@W","alchemaniac","YUJI","HUG","戏言咸咸","下野宏铭","LLC","REALMBW","我妻洛酱","幻象黑兔","LM7","渣念","鸭","RAN","Lanzi","aZLing4","deel","时辰","Lpip","Iritoa","KENTllaall","Anmi","将","咩煲","藻","STAR影法师","KuroBlood","toast","")
 # infrapls=("无","控制中枢","制造站","贸易站","发电站","宿舍","会客室","办公室","训练室","加工站")
-# infraspecial=("无","效率加","效率减","扩容/增量","减容量","心情消耗减少（恢复加快）","心情消耗增加（恢复减慢）","针对特殊干员生效","仅对自身有效","仅对同设施其他干员有效","针对特殊物品加成","加成会随时间变化","加成恒定","","",)
+# infraspecial=("无","效率（副产品百分比）加","效率（副产品百分比）减","扩容/增量","减容量","心情消耗减少（恢复加快）","心情消耗增加（恢复减慢）","针对特殊干员生效","仅对自身有效","仅对同设施其他干员有效","针对特殊物品加成","加成会随时间变化","加成恒定")
 # tagtuple=("不可公招","近卫干员","术士干员","医疗干员","特种干员","狙击干员","先锋干员","辅助干员","重装干员","新手","资深干员","高级资深干员","远程位","近战位","治疗","支援","输出","群攻","减速","生存","防护","削弱","位移","控场","爆发","召唤","快速复活","费用恢复","支援机械")
 # racetuple=("菲林（猫科）","鬼","埃拉菲亚（梅花鹿）","丰蹄（）","萨弗拉（）","","","","","","","","","未公开")
 # xp1file content:["",,[,],"",,,,,],
@@ -36,10 +36,10 @@ import os,time,xlsxwriter
 typetuple=("近卫","术师","医疗","特种","狙击","先锋","辅助","重装")
 organizationtuple=("罗德岛","喀兰贸易","龙门","莱茵生命","格拉斯哥帮","使徒","汐斯塔","深海猎人","SWEEP","乌萨斯学生自治团","王者之杖","企鹅物流","黑钢国际","维多利亚","卡西米尔无胄盟","莱塔尼亚")
 drawertuple=("Liduke","竜崎いち","虎三","Infukun","海猫络合物","Skade","NoriZC","m9nokuro","一立里子","谜肘","TOKI","neco","阿鬼","唯@W","alchemaniac","YUJI","HUG","戏言咸咸","下野宏铭","LLC","REALMBW","我妻洛酱","幻象黑兔","LM7","渣念","鸭","RAN","Lanzi","aZLing4","deel","时辰","Lpip","Iritoa","KENTllaall","Anmi","将","咩煲","藻","STAR影法师","KuroBlood","toast","")
-vartuple=("姓名","身高","生日","外文名","职业","性别","星级","所属组织","画师",)
+vartuple=("姓名","身高","生日","外文名","职业","性别","星级","所属组织","画师","基建位置","基建加成","公招tag","种族")
 infrapls=("无","控制中枢","制造站","贸易站","发电站","宿舍","会客室","办公室","训练室","加工站")
 tagtuple=("不可公招","近卫干员","术士干员","医疗干员","特种干员","狙击干员","先锋干员","辅助干员","重装干员","新手","资深干员","高级资深干员","远程位","近战位","治疗","支援","输出","群攻","减速","生存","防护","削弱","位移","控场","爆发","召唤","快速复活","费用恢复","支援机械")
-
+infraspecial=("无","效率加","效率减","扩容/增量","减容量","心情消耗减少（恢复加快）","心情消耗增加（恢复减慢）","针对特殊干员生效","仅对自身有效","仅对同设施其他干员有效","针对特殊物品加成","加成会随时间变化","加成恒定")
 
 
 def getday(datelist):
@@ -296,6 +296,16 @@ def startsearch():
 	tmp=dat;out=[]
 	if "name" in searchdict:
 		tmp1=searchdict["name"]
+			if x[3] in tmp1:
+				out.append(x)
+		tmp=out[:];out=[]
+	if "type" in searchdict:
+		tmp1=searchdict["type"]
+		for x in tmp:
+			if x[4] in tmp1:
+				out.append(x)
+		tmp=out[:];out=[]
+	if "gender" in searchdict:
 		if "*" in tmp1:
 			if optionlist[4]:
 				tmp=sort_max(tmp)
@@ -374,6 +384,34 @@ def startsearch():
 		tmp1=searchdict["drawer"]
 		for x in tmp:
 			if x[8] in tmp1:
+				out.append(x)
+		tmp=out[:];out=[]
+	if "infrapls" in searchdict:
+		tmp1=searchdict["infrapls"]
+		for x in tmp:
+			if x[9]==tmp1:
+				out.append(x)
+		tmp=out[:];out=[]
+	if "infraspecial" in searchdict:
+		tmp1=searchdict["infraspecial"]
+		for x in tmp:
+			if x[10]==tmp1:
+				out.append(x)
+		tmp=out[:];out=[]
+	if "tag" in searchdict:
+		tmp1=searchdict["tag"]
+		for x in tmp:
+			c=0
+			for y in tmp1:
+				if y in x[11]:
+					c+=1
+			if c=len(tmp1):
+				out.append(x)
+		tmp=out[:];out=[]
+	if "race" in searchdict:
+		tmp1=searchdict["race"]
+		for x in tmp:
+			if x[12] in tmp1:
 				out.append(x)
 		tmp=out[:];out=[]
 	if optionlist[4]:
@@ -660,6 +698,24 @@ if 9 in l1:    #画师
 		tmp[tmp.index(x)]=x-1
 	searchdict["drawer"]=tmp
 
+if 10 in l1:   #基建位置
+	os.system("cls")
+	print("下列多选选项为“和”的关系，将会被作为一个查询关键字查询")
+	searchdict["infrapls"]=getchoice(infrapls,True)
+
+if 11 in l1:   #基建加成
+	os.system("cls")
+	print("下列多选选项为“和”的关系，将会被作为一个查询关键字查询")
+	searchdict["infraspecial"]=getchoice(infraspecial,True)
+
+if 12 in l1:   #公招tag
+	os.system("cls")
+	print("下列多选选项为“和”的关系，将会被作为一个查询关键字查询")
+	searchdict["tag"]=getchoice(tagtuple,True)
+
+if 13 in l1:   #种族
+	os.system("cls")
+	searchdict["race"]=getchoice(racetuple,True)
 
 os.system("cls")
 if optionlist[2]:
